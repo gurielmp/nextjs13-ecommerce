0795f110721ae83b6c04337da7eb1c06f5b5aeea
gurielamp <gurielamp@gmail.com>
2023-10-20 20:07:49 +0700

    add button component

---
+++ src/components/FormSubmitButton.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ComponentProps } from "react";
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
+
+type FormSubmitButtonProps = {
+  children: React.ReactNode;
+  className?: string;
+} & ComponentProps<"button">;
+
+export default function FormSubmitButton({
+  children,
+  className,
+  ...props
+}: FormSubmitButtonProps) {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      {...props}
+      type="submit"
+      disabled={pending}
+      className={`btn btn-primary ${className}`}
+    >
+      {pending && <span className="loading loading-spinner"></span>}
+      {children}
+    </button>
+  );
+}
