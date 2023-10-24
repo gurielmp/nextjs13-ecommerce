c910623bd3925381e4f2e9f31f9a7893abe0f25d
gurielamp <gurielamp@gmail.com>
2023-10-24 17:29:48 +0700

    create navbar and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "./NavBar/NavBar";
+import Footer from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Guriel | E-Commerce",
+  title: "Guriel | Commerce",
   description: "We provides the best products for you",
 };
 
@@ -17,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NavBar />
         <main className="m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
+        <Footer />
       </body>
     </html>
   );
